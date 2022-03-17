082592712ba67d86620ba2fdb8b2d08451dd6659
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-03-17 14:06:59 +0900

    add: movies/models.py 작성

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Actor(models.Model):
+    first_name    = models.CharField(max_length=45)
+    last_name     = models.CharField(max_length=45)
+    date_of_birth = models.DateField()
+    movies        = models.ManyToManyField('Movie', through='ActorMovie')
+
+    class Meta:
+        db_table = 'actors'
+
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    release_Date = models.DateField()
+    running_time = models.IntegerField()
+
+    class Meta:
+        db_table = 'movies'
+
+class ActorMovie(models.Model):
+    actor = models.ForeignKey('Actor',on_delete=models.CASCADE)
+    movie = models.ForeignKey('Movie',on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'actormovie'
